e1f2630291c42c159a8e12fe31b63872e956c3b1
Sirc13 <kurylevsasha200@gmail.com>
2025-08-16 08:54:44 +0300

    feat(CalendarCheck): Добавил модели Data, DataRequest, DataResponse
    feat(CalendarCheck): Добавил pom.xml и добавил в него зависимости

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,4 +1,12 @@
 package project;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+    public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
---
+++ src/main/java/project/model/Data.java
@@ -0,0 +1,15 @@
+package project.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Data {
+    private String data;
+}
