ded6cf4bbd92130d8053a1f199bb8002bab25698
Sirc13 <kurylevsasha200@gmail.com>
2025-08-24 15:33:32 +0300

    feat(CalendarCheck): Добавил DataResponse

---
+++ src/main/java/project/model/DataResponse.java
@@ -0,0 +1,15 @@
+package project.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DataResponse {
+    private boolean success;
+}
+
