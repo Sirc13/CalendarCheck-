55b94ca4f747f635ec65462d7e7fae95678c4154
Sirc13 <kurylevsasha200@gmail.com>
2025-08-24 15:57:34 +0300

    fix(CalendarCheck): пофиксил контроллер

--- src/main/java/project/controller/DataController.java
+++ src/main/java/project/controller/DataController.java
@@ -12,14 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 import project.model.DataRequest;
 
 import project.model.DataResponse;
+import project.service.DataService;
 
 @RestController
 @RequestMapping("/api/data")
 @RequiredArgsConstructor
 public class DataController {
-
+    private final DataService dataService;
     @GetMapping("/work")
     public DataResponse getSuccessWorkedDay(DataRequest dataRequest) {
-        return null;
+        return dataService.getSuccessWorkedDay(dataRequest);
     }
 }
